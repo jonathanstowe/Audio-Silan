62b05d90a457cdefa4aff539a5549275d39d1e18
Jonathan Stowe <jns@gellyfish.co.uk>
2019-01-12 10:51:51 +0000

    Make meta test optional on presence of Test::META

--- lib/Audio/Silan.pm
+++ lib/Audio/Silan.pm
@@ -85,7 +85,7 @@ audio is detected to end.
 
 =end pod
 
-class Audio::Silan:ver<0.0.5>:auth<github:jonathanstowe>:api<1.0> {
+class Audio::Silan:ver<0.0.6>:auth<github:jonathanstowe>:api<1.0> {
     use File::Which;
     use JSON::Fast;
 
